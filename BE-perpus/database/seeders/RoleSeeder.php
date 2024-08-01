ac9b762629b3ba2ca0609033bfdb7e577f35eb8a
AnnaasLuthfi <annaasluthfi@gmail.com>
2024-08-01 13:36:19 +0700

    FE

--- BE-perpus/database/seeders/RoleSeeder.php
+++ BE-perpus/database/seeders/RoleSeeder.php
@@ -14,7 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Roles::create([
-            'name' => 'ownwe',
+            'name' => 'owner',
         ]);
 
         Roles::create([
